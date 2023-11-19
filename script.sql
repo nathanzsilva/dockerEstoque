fccab01f924a87fa5de725d374049293ba17badf
Nathan Silva <nathan.silva26@gmail.com>
2023-11-18 23:56:14 -0300

    subindo configuracoes da tabela

---
+++ script.sql
@@ -0,0 +1,57 @@
+create database  EstoqueNV;
+
+USE EstoqueNV
+CREATE TABLE Corredor (
+    codigo INT IDENTITY(1,1) PRIMARY KEY,
+    descricao VARCHAR(200)
+);
+
+CREATE TABLE Local (
+    codigo INT IDENTITY(1,1) PRIMARY KEY,
+    descricao VARCHAR(200)
+);
+
+CREATE TABLE Andar (
+    codigo INT IDENTITY(1,1) PRIMARY KEY,
+    descricao VARCHAR(200)
+);
+
+CREATE TABLE Locacao (
+    codigo INT IDENTITY(1,1) PRIMARY KEY,
+    CorredorCodigo INT,
+    LocalCodigo INT,
+    AndarCodigo INT,
+    isAtivo BIT,
+    FOREIGN KEY (CorredorCodigo) REFERENCES Corredor(codigo),
+    FOREIGN KEY (LocalCodigo) REFERENCES tabLocal(codigo),
+    FOREIGN KEY (AndarCodigo) REFERENCES Andar(codigo)
+);
+
+create table Usuario(
+codigo int primary key identity(1,1),
+nome varchar(150) not null,
+email varchar(150) not null,
+senha varchar(50) not null,
+cargoCodigo int not null,
+)
+
+create table Cargo(
+codigo int primary key identity(1,1),
+descricao varchar(100) not null
+)
+CREATE TABLE Produtos (
+    codigo INT IDENTITY(1,1) PRIMARY KEY,
+    nomeProduto VARCHAR(200),
+    valor DECIMAL(17,2),
+    Quantidade INT,
+    DataVencimento DATETIME,
+    LocacaoCodigo INT, -- Renomeando para evitar conflito de nome
+    FOREIGN KEY (LocacaoCodigo) REFERENCES tabLocacao(codigo)
+);
+create table ProdutosBaixa(
+codigo int primary key identity(1,1),
+usuarioCodigo int not null,
+produtoCodigo int not null,
+constraint FK_USUARIO_PRODUTOBAIXA foreign key(usuarioCodigo) references Usuario(codigo),
+constraint FK_PRODUTO_PRODUTOBAIXA foreign key(usuarioCodigo) references Usuario(codigo),
+)
